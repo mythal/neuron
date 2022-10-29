ff6d6ebc4a69602a695446f1a09bcd78de7715eb
ギャラ <me@gyara.moe>
2022-10-29 22:00:33 +0900

    feat: ping!

--- app.ts
+++ app.ts
@@ -1,3 +1,18 @@
-import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { sendMessage } from "./core/mod.ts";
 
-serve((req: Request) => new Response("Hello World"));
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .get("/ping", async (context) => {
+    await sendMessage("pong");
+    context.response.body = "done";
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8080 });
