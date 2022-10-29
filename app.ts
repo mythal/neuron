841150a4c9d1e787022e21f0fbac480afc02ba75
ギャラ <me@gyara.moe>
2022-10-29 22:06:21 +0900

    feat: Pin oak.

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { sendMessage } from "./core/mod.ts";
 
 const router = new Router();
